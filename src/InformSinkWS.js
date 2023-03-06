f9de4878b260035b282b51953f0488e081c768b1
interrupter <alex.repin@mail.ru>
2023-03-06 12:27:31 +0300

    added: multi clients deployment

--- src/InformSinkWS.js
+++ src/InformSinkWS.js
@@ -1,40 +1,47 @@
-const log = require('not-log')(module, 'InformSinkWS');
-try{
-	const
-		notNode = require('not-node'),
-		Sink = require('not-inform').Sink;
+const log = require("not-log")(module, "InformSinkWS");
+try {
+    const notNode = require("not-node"),
+        Sink = require("not-inform").Sink;
 
-	class InformSinkWS extends Sink{
-		/**
-		* @param {object} options
-		*/
-		constructor(options){
-			super(options);
-			return this;
-		}
+    class InformSinkWS extends Sink {
+        /**
+         * @param {object} options
+         */
+        constructor(options) {
+            super(options);
+            return this;
+        }
 
-		async deploy(message, rule){
-			try{
-				if(this.options.eventName){
-					await this.deployCycle(message, rule);
-				}
-			}catch(e){
-				log.error(e);
-			}
-		}
+        async deploy(message, rule) {
+            try {
+                if (this.options.eventName) {
+                    await this.deployCycle(message, rule);
+                }
+            } catch (e) {
+                log.error(e);
+            }
+        }
 
-		async deployOne({message, recipient,  index, recipientsFilter, rule}){
-			const server = notNode.Application.WSServer();
-			if(server && recipient._id){
-				const client = server.getClient({_id: recipient._id});
-				if(client){
-					client.send('event', this.options.eventName, message);
-				}
-			}
-		}
-	}
+        async deployOne({ message, recipient, index, recipientsFilter, rule }) {
+            const server = notNode.Application.WSServer();
+            if (server && recipient._id) {
+                const clients = server.getClients({ _id: recipient._id });
+                if (clients && clients.length) {
+                    clients.forEach(
+                        this.deliver(this.options.eventName, message)
+                    );
+                }
+            }
+        }
 
-	module.exports = InformSinkWS;
-}catch(e){
-	log.error(e);
+        deliver(eventName, message) {
+            return (client) => {
+                client.send("event", eventName, message);
+            };
+        }
+    }
+
+    module.exports = InformSinkWS;
+} catch (e) {
+    log.error(e);
 }
